1ceb660bf013311c8bad4ae1d94748e0bce3bad1
Konkanok2 <konkanok4452@gmail.com>
2024-10-16 14:23:44 +0700

    Create report_shop.php

---
+++ signin/report_shop.php
@@ -0,0 +1,501 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <style>
+        :root {
+            --orange: #ff7800;
+            --black: #130f40;
+            --white: #fff;
+            --light-color: #666;
+            --box-shadow: 0 .5rem 1rem rgba(0, 0, 0, .1);
+            --border: .2rem solid rgba(0, 0, 0, .1);
+            --outline: .1rem solid rgba(0, 0, 0, .1);
+            --outline-hover: .2rem solid var(--black);
+        }
+
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            outline: none;
+            border: none;
+            text-decoration: none;
+            text-transform: capitalize;
+            transition: all .2s linear;
+            font-family: 'Poppins', sans-serif;
+        }
+
+        html {
+            font-size: 62.5%;
+            overflow-x: hidden;
+            scroll-behavior: smooth;
+            scroll-padding-top: 7rem;
+        }
+
+        body {
+            background: #eee;
+        }
+
+        section {
+            padding: 2rem 9%;
+        }
+
+        .btn {
+            display: inline-block;
+            padding: .8rem 3rem;
+            font-size: 1.7rem;
+            border-radius: .5rem;
+            border: .2rem solid var(--black);
+            color: var(--black);
+            cursor: pointer;
+            text-align: center;
+        }
+
+        .btn:hover {
+            background: #507F99;
+            color: #fff;
+        }
+
+        .header {
+            position: fixed;
+            top: 0;
+            left: 0;
+            right: 0;
+            z-index: 1000;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            padding: 2rem 9%;
+            background: var(--white);
+            box-shadow: var(--box-shadow);
+        }
+
+        .header .logo {
+            font-size: 2.5rem;
+            font-weight: bold;
+            color: var(--black);
+        }
+
+        .header .navbar.active {
+            right: 2rem;
+            transition: .4s linear;
+        }
+
+        .header .navbar a {
+            font-size: 1.7rem;
+            margin: 0 1rem;
+            color: var(--black);
+        }
+
+        .header .navbar a:hover {
+            color: #507F99;
+        }
+
+        .header .icons div {
+            height: 4.5rem;
+            width: 4.5rem;
+            line-height: 4.5rem;
+            border-radius: .5rem;
+            background: #fff;
+            color: var(--black);
+            font-size: 2rem;
+            margin-right: .3rem;
+            cursor: pointer;
+            text-align: center;
+        }
+
+        .header .icons div:hover {
+            background: #507F99;
+            color: #fff;
+        }
+
+        #menu-btn {
+            display: none;
+        }
+
+        .header .search-form {
+            position: absolute;
+            top: 110%;
+            right: -110%;
+            width: 50rem;
+            height: 5rem;
+            background: var(--white);
+            border-radius: .5rem;
+            overflow: hidden;
+            display: flex;
+            align-items: center;
+            box-shadow: var(--box-shadow);
+        }
+
+        .header .search-form.active {
+            right: 2rem;
+            transition: .4s linear;
+        }
+
+        .header .search-form input {
+            height: 100%;
+            width: 100%;
+            background: none;
+            font-size: 1.6rem;
+            color: var(--black);
+            padding: 0 1.5rem;
+        }
+
+        .header .search-form label {
+            font-size: 2.2rem;
+            padding-right: 1.5rem;
+            color: var(--black);
+            cursor: pointer;
+        }
+
+        .header .search-form label:hover {
+            color: #507F99;
+        }
+
+
+
+        .header .profile {
+            position: absolute;
+            top: 110%;
+            right: -110%;
+            padding: 1rem;
+            border-radius: .5rem;
+            box-shadow: var(--box-shadow);
+            width: 25rem;
+            background: var(--white);
+            text-align: center;
+        }
+
+        .header .profile.active {
+            right: 2rem;
+            transition: .4s linear;
+        }
+
+        .header .profile img {
+            width: 50px;
+            height: 50px;
+            border-radius: 50%;
+            object-fit: cover;
+            margin-bottom: .5rem;
+        }
+
+        .header .profile h3 {
+            font-size: 1.8rem;
+            color: var(--black);
+            margin-bottom: .5rem;
+        }
+
+        .header .profile span {
+            font-size: 1.4rem;
+            color: var(--light-color);
+        }
+
+        .header .profile .btnp {
+            display: block;
+            width: 100%;
+            text-align: center;
+            background: #507F99;
+            color: #fff;
+            padding: .8rem 0;
+            margin-top: 1rem;
+            border-radius: .5rem;
+            font-size: 1.7rem;
+        }
+
+        .header .profile .btnp:hover {
+            background: #728FCE;
+        }
+
+        .header .profile .flex-btnp {
+            display: flex;
+            justify-content: space-between;
+            margin-top: 1rem;
+        }
+
+        .header .profile .option-btnp {
+            flex: 1;
+            text-align: center;
+            padding: .8rem 0;
+            background: #507F99;
+            color: var(--white);
+            font-size: 1.4rem;
+            border-radius: .5rem;
+            margin: 0 .5rem;
+            transition: all .3s ease;
+        }
+
+        .header .profile .option-btnp:hover {
+            background: #728FCE;
+            color: #fff;
+        }
+
+        @media (max-width:991px) {
+            html {
+                font-size: 55%;
+            }
+
+            .header {
+                padding: 2rem;
+            }
+
+            section {
+                padding: 2rem;
+            }
+
+        }
+
+        @media (max-width: 768px) {
+            .header .search-form {
+                width: 90%;
+            }
+
+            #menu-btn {
+                display: inline-block;
+            }
+
+            .header .navbar {
+                position: absolute;
+                top: 100%;
+                right: -110%;
+                width: 30rem;
+                box-shadow: var(--box-shadow);
+                border-radius: .5rem;
+                background: #fff;
+            }
+
+            .header .navbar.active {
+                right: 2rem;
+                transition: .4s linear;
+            }
+
+            .header .navbar a {
+                font-size: 2rem;
+                margin: 2rem 2.5rem;
+                display: block;
+            }
+
+            
+
+        }
+
+        @media (max-while:450px) {
+            html {
+                font-size: 50%;
+            }
+
+            .form-select {
+                padding: .8rem 0;
+                margin-top: 1rem;
+                border-radius: .5rem;
+                font-size: 1.7rem;
+                border: 1px solid #507F99;
+            }
+
+            .dlp {
+                text-align: center;
+                background: #130f40;
+                color: #fff;
+                padding: .8rem 0;
+                margin-top: 1rem;
+                border-radius: .5rem;
+                font-size: 1.7rem;
+            }
+
+            .dlp:hover {
+                background-color: #507F99;
+            }
+
+        }
+
+        /*ย้อนหลัง*/
+        .font {
+            margin-top: 100px;
+            font-size: 12px;
+            margin-left: 20px;
+        }
+
+        /*ปุ่ม*/
+        .dlp {
+            text-align: center;
+            background: #130f40;
+            color: #fff;
+            padding: .8rem 0;
+            margin-top: 1rem;
+            border-radius: .5rem;
+            font-size: 1.7rem;
+        }
+
+        .dlp:hover {
+            background-color: #507F99;
+        }
+
+        .form-select {
+            padding: .8rem 0;
+            margin-top: 1rem;
+            border-radius: .5rem;
+            font-size: 1.7rem;
+            border: 1px solid #507F99;
+            margin-bottom: 5px;
+        }
+
+        .date label {
+            font-size: 1.7rem;
+        }
+
+        .from_group label {
+            font-size: 1.7rem;
+        }
+
+        input[type="time"],
+        input[type="date"] {
+            padding: 10px 20px;
+            margin: 0 5px;
+            outline: none;
+            border: 1px solid #507F99;
+            border-radius: 6px;
+            color: #0298cf;
+            font-size: 14px;
+            background-color: #fff;
+            margin-bottom: 5px;
+        }
+
+
+        
+    </style>
+    <title>Report</title>
+</head>
+
+<body>
+
+    <!-- header section starts -->
+    <header class="header">
+        <a href="dashboard_shop.php" class="logo">Laundry</a>
+
+        <nav class="navbar">
+            <a href="dashboard_shop.php">Dashboard</a>
+            <a href="store_shop.php">Shop</a>
+            <a href="faq_shop.php">FAQ</a>
+            <a href="sevice_shop.php">Terms & Condition</a>
+        </nav>
+
+        <div class="icons">
+            <div class="bx bx-menu" id="menu-btn"></div>
+            <div class="bx bx-search" id="search-btn"></div>
+            <div class="bx bx-user" id="user-btn"></div>
+        </div>
+
+        <form action="" class="search-form">
+            <input type="search" id="search-box" placeholder="Search here...">
+            <label for="search-box" class="bx bx-search"></label>
+        </form>
+
+
+        <div class="profile">
+            <?php
+            // ตรวจสอบว่ามีรูปภาพหรือไม่ ถ้าไม่มีให้แสดงรูปภาพเริ่มต้น
+            $shopimg = !empty($shop['shop_img']) ? htmlspecialchars($shop['shop_img']) : 'default.jpg';
+            ?>
+            <img src="../photo/<?php echo $shopimg; ?>" alt="Profile Image">
+            <h3><?php echo $fullName; ?></h3>
+            <span>shop</span>
+            <a href="profile_shop.php" class="btnp">Profile</a>
+            <div class="flex-btnp">
+                <a href="report_shop.php" class="option-btnp">รายงาน</a>
+                <a href="login.php" class="option-btnp">Logout</a>
+            </div>
+        </div>
+    </header>
+
+    <!-- รายงาน -->
+
+    <div class="container">
+
+        <div class="font">
+            <h1>รายงาน</h1>
+
+            <select class="form-select" aria-label="Default select example">
+                <option selected>รายการ</option>
+                <option value="1">ยอดขาย</option>
+                <option value="2">ออเดอร์</option>
+                <option value="3">บริการ</option>
+            </select>
+            <br>
+            <div class="date">
+                <label for="text" type="date">วันที่เริ่มต้น - สิ้นสุด</label>
+                <input type="date"><span> - </span>
+                <input type="date">
+            </div>
+            <div class="from_group">
+                <label for="time_1" type="time">เวลาเริ่มต้น - สิ้นสุด</label>
+                <input type="time" name="time" id="time"> <span> - </span>
+                <input type="time" name="time" id="time">
+            </div>
+
+            <button class="dlp">Download PDF</button>
+
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+    <!--custom js file link -->
+    <script src="js/script.js"></script>
+
+    <script>
+        let searchForm = document.querySelector('.search-form');
+        let profile = document.querySelector('.header .profile');
+        let navbar = document.querySelector('.header .navbar');
+
+        document.querySelector('#search-btn').onclick = () => {
+            searchForm.classList.toggle('active');
+            profile.classList.remove('active');
+            navbar.classList.remove('active');
+        }
+
+        document.querySelector('#user-btn').onclick = () => {
+            profile.classList.toggle('active');
+            searchForm.classList.remove('active');
+            navbar.classList.remove('active');
+        }
+
+        document.querySelector('#menu-btn').onclick = () => {
+            navbar.classList.toggle('active');
+            searchForm.classList.remove('active');
+            profile.classList.remove('active');
+        }
+
+        window.onscroll = () => {
+            searchForm.classList.remove('active');
+            profile.classList.remove('active');
+            navbar.classList.remove('active');
+        }
+    </script>
+</body>
+
+</html>
